9906abf27542f54c1ea8d6a366012f038cd7b67d
zarakhan024 <zarakhann2417@gmail.com>
2025-08-15 13:48:46 +0300

    adding main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js') // Optional for later
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
